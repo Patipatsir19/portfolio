a601cbe92e7da647cce3c8b645650164ba567a4a
Patipatsir19 <patipat.sir@gmail.com>
2022-11-18 20:43:54 +0700

    make certificate page

--- src/pages/Certificate/certificate.component.jsx
+++ src/pages/Certificate/certificate.component.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+
+import './certificate.styles.scss';
 
 const Certificate = () => {
   return (
-    <div>Certificate</div>
-  )
-}
+    <div className="cer">
+      <div className="cer_list">
+        <ul>
+          <li>Retort Supervisor</li>
+          <li>
+            This course train the people who work in retort section in factory.<br/>
+            Learnt important knowledge for retort section and had exchanged
+            experience woking.
+          </li>
+          <li>03/2021</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default Certificate;
